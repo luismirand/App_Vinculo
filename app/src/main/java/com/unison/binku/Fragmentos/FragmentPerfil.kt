8b8111e6a40db52f02915d797f2d7df7a57817ac
Alan Lizardi <gil.lizardi@gmail.com>
2025-11-03 22:25:58 -0700

    placeholder de perfil actualizado

--- app/src/main/java/com/unison/binku/Fragmentos/FragmentPerfil.kt
+++ app/src/main/java/com/unison/binku/Fragmentos/FragmentPerfil.kt
@@ -91,11 +91,11 @@ class FragmentPerfil : Fragment() {
                     if (imagen.isNotEmpty() && imagen != "null") {
                         Glide.with(mContext)
                             .load(imagen)
-                            .placeholder(R.drawable.ic_login)// Placeholder mientras carga
+                            .placeholder(R.drawable.ic_perfil_black)// Placeholder mientras carga
                             .into(binding.TvPerfil) // Asegúrate que TvPerfil sea un ImageView
                     } else {
                         // Si no hay imagen, poner el placeholder directamente
-                        binding.TvPerfil.setImageResource(R.drawable.ic_login)
+                        binding.TvPerfil.setImageResource(R.drawable.ic_perfil_black)
                     }
                 } catch (e: Exception){
                     Toast.makeText(mContext, "${e.message}", Toast.LENGTH_SHORT).show()
